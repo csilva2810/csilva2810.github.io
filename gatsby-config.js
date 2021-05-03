450887f45dd7dde38d300249986bfcf7237ba076
Carlos Silva <csilva2810@gmail.com>
2021-05-03 09:56:29 -0300

    Improve SEO

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,11 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: `Carlos Silva's Blog`,
+    description: `Carlos Silva's Blog home page`,
+    author: `carlossilva`,
+  },
   plugins: [
     {
       resolve: "gatsby-plugin-typography",
@@ -13,5 +18,18 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Carlos Silva's Blog`,
+        short_name: `Carlos' Blog`,
+        start_url: `/`,
+        background_color: `#00cba9`,
+        theme_color: `#00cba9`,
+        display: `standalone`,
+        icon: `src/assets/images/favicon.png`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,12 @@ import React from "react"
 import Header from "../components/header"
 import Profile from "../components/profile"
 import Hero from "../components/hero"
+import SEO from "../components/seo"
 
 export default function Home() {
   return (
     <>
+      <SEO title="Home" />
       <Header />
       <Hero>
         <Profile />
